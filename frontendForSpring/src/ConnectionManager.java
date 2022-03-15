9c02cf69296ef9f05a00b4de2c2f550400efd18a
LisaDuente <lisa.duente@gmail.com>
2022-03-15 12:00:34 +0100

    frontend for spring backend

---
+++ frontendForSpring/src/ConnectionManager.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ConnectionManager {
+
+    //object to couple the program to a URL
+    private HttpURLConnection connection;
+
+    public String sendRequest(String request){
+        String line = "";
+        String response = "";
+        try {
+            URL url = new URL("http://localhost:8080/hello"+request);
+            this.connection = (HttpURLConnection) url.openConnection();
+
+            //choose what you want to do, just now we want to get something
+            this.connection.setRequestMethod("GET");
+            //set a timeout, if you cant create a connection within this time, break connection
+            this.connection.setConnectTimeout(5000);
+            //same for read
+            this.connection.setReadTimeout(5000);
+
+            //every status under 300 no error, we want to have 200(OK)
+            int status = connection.getResponseCode();
+            System.out.println("status: "+status);
+
+            if(status < 300){
+                //we want to have a stream if the status is ok,
+                //to handle the input stream we gonna read it with an inputStreamReader
+                //to handle it the best way, lay it in a bufferedReader
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                //concat everything in response
+                while((line = reader.readLine())!= null){
+                    response += line;
+                }
+                reader.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
---
+++ frontendForSpring/src/GUI.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+
+//for this program to run you need to run a RESTful backend program at the same time
+
+public class GUI extends JFrame {
+    private JTextField nameInput;
+    private JTextField num1Input;
+    private JTextField num2Input;
+    private JButton enterName;
+    private JButton enterNumbers;
+    private JLabel resultName;
+    private JLabel resultNumbers;
+    private JLabel plus;
+    private ConnectionManager manager;
+
+    public GUI(){
+        this.manager = new ConnectionManager();
+
+    //define output for hello method
+        this.nameInput = new JTextField();
+        this.enterName = new JButton("Enter");
+        this.resultName = new JLabel("Empty");
+
+        this.nameInput.setBounds(50,50,115,25);
+        this.enterName.setBounds(170,50,75,25);
+        this.resultName.setBounds(75,75,200,25);
+
+        this.enterName.addActionListener((e) -> {
+                    String text = manager.sendRequest("/?name="+nameInput.getText());
+                    nameInput.setText("");
+                    resultName.setText(text);
+        });
+
+
+    //define output for calculate
+        this.num1Input = new JTextField();
+        this.num2Input = new JTextField();
+        this.enterNumbers = new JButton("Enter");
+        this.resultNumbers = new JLabel("Empty");
+        this.plus = new JLabel("+");
+
+        this.num1Input.setBounds(50,175,50,25);
+        this.num2Input.setBounds(115,175,50,25);
+        this.plus.setBounds(103,175,20,25);
+        this.enterNumbers.setBounds(170,175,75,25);
+        this.resultNumbers.setBounds(75,200,200,25);
+
+    //add to the frame
+        this.add(num1Input);
+        this.add(num2Input);
+        this.add(plus);
+        this.add(enterNumbers);
+        this.add(resultNumbers);
+        this.add(enterName);
+        this.add(nameInput);
+        this.add(resultName);
+
+        this.setSize(300,300);
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+}
